8b14d47bc046a489a54b25a5d9f82e7bab1b9871
bzhao41@gmail.com <bzhao41@gmail.com@2fade78a-b14e-622e-b5b6-8f4446781d8b>
2012-02-15 00:46:54 +0000

    Comments

--- trunk/src/team3929/commands/BallIntake/CheckBalls.java
+++ trunk/src/team3929/commands/BallIntake/CheckBalls.java
@@ -15,7 +15,7 @@ public class CheckBalls extends CommandBase {
     public CheckBalls() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-        requires(ballIntake);
+        requires(ballIntake); //Requires the ballIntake Subsystem
     }
 
     // Called just before this Command runs the first time
@@ -24,20 +24,20 @@ public class CheckBalls extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if (oi.checkButton(9) == true) {
-            ballIntake.turnOnConveyorBelts();
-        } else if (oi.checkButton(10) == true) {
-            ballIntake.turnOffConveyorBelts();
+        if (oi.checkButton(9) == true) { //Method from the oi class that checks if the button is pressed and if it is pressed
+            ballIntake.turnOnConveyorBelts();//then uses the turnOnConveyorBelts method from ballIntake
+        } else if (oi.checkButton(10) == true) { //Method from the oi class that checks if the button is pressed and if it is pressed
+            ballIntake.turnOffConveyorBelts();//then turn off the conveyor belts
         }
-        ballIntake.checkEnterLightSensor();
-        ballIntake.checkShooterSensor();
-        System.out.println(ballIntake.ballCounter);           // System.out.println(ballIntake.ballCounter);
+        ballIntake.checkEnterLightSensor(); //calls the method to check theentrance light sensor
+        ballIntake.checkShooterSensor();//calls the method to check the shooter light sensor
+        System.out.println(ballIntake.ballCounter);//Prints out the value for the ballCounter int in ballIntake class
 
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return false;//Never returns true
     }
 
     // Called once after isFinished returns true
