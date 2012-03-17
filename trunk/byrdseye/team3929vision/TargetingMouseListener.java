326bb94b0a2347c4364bece0fce3bdf8f870d1e9
bhgray@gmail.com <bhgray@gmail.com@2fade78a-b14e-622e-b5b6-8f4446781d8b>
2012-03-17 14:09:51 +0000

    added byrdseye as a separate directory under the trunk

---
+++ trunk/byrdseye/team3929vision/TargetingMouseListener.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team3929.vision;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author bhgray
+ */
+public class TargetingMouseListener implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
